eafa3448ba008ff491b153e8425e98928bd7318c
Rene alejandro Hernandez Ramirez <Alexramires2022@outlook.es>
2024-03-07 12:15:28 -0600

    Access-Job-visual4

--- Access Job/Controllers/HomeController.cs
+++ Access Job/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
 
 namespace Access_Job.Controllers
 {
@@ -25,6 +26,42 @@ namespace Access_Job.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult SaveContact(string nombre, string correo, string telefono, string mensaje)
+        {
+            string connectionString = "Server=localhost;Port=3308;Database=formulario_contacto;Uid=root;Pwd=;";
+
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO 0 (nombre, correo, telefono, mensaje) VALUES (@Nombre, @Correo, @Telefono, @Mensaje)";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Nombre", nombre);
+                        command.Parameters.AddWithValue("@Correo", correo);
+                        command.Parameters.AddWithValue("@Telefono", telefono);
+                        command.Parameters.AddWithValue("@Mensaje", mensaje);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+                        ViewBag.SuccessMessage = "¡Tu mensaje ha sido enviado con éxito!";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = "Ha ocurrido un error al enviar el mensaje: " + ex.Message;
+                Console.WriteLine(ex.ToString()); // Agregar esta línea para imprimir el mensaje de error en la consola
+            }
+
+
+            return View("Index");
         }
     }
 }
---
+++ SQLQuery2.sql
@@ -0,0 +1,10 @@
+﻿CREATE DATABASE bdcontactos;
+use bdcontactos;
+CREATE TABLE contactos(
+	Idcontactos bigint PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	Nombre varchar(50) NOT NULL,
+    Telefono varchar(10) NOT NULL,
+    Correo varchar(60) NOT NULL, 
+	mensaje TEXT NOT NULL
+	);
+
