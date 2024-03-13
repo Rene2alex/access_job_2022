46db53c5828128fb4c36226851fef06e79dfbe17
Rene alejandro Hernandez Ramirez <Alexramires2022@outlook.es>
2024-03-13 11:24:27 -0600

    Access-Job-visual9

--- Access Job/Controllers/HomeController.cs
+++ Access Job/Controllers/HomeController.cs
@@ -25,12 +25,12 @@ namespace Access_Job.Controllers
             return View();
         }
 
-
         [HttpPost]
         public ActionResult Index(string nombre, string correo, string telefono, string mensaje)
         {
             string connectionString = "Server=localhost;Port=3308;Database=formulariop;Uid=root;Pwd=;";
 
+            // Insertar en la base de datos
             using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
                 string query = "INSERT INTO contactos (nombre, correo, telefono, mensaje) VALUES (@Nombre, @Correo, @Telefono, @Mensaje)";
@@ -44,35 +44,20 @@ namespace Access_Job.Controllers
             }
 
             // Enviar correo electrónico
-            EnviarCorreo(correo, nombre, telefono, mensaje);
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("alfredodanielramos123456@gmail.com"); // Coloca tu dirección de correo electrónico
+            mail.To.Add("Anyi.moralesmtz@gmail.com");
+            mail.Subject = "Nuevo mensaje desde el formulario";
+            mail.Body = $"Nombre: {nombre}\nCorreo: {correo}\nTeléfono: {telefono}\nMensaje: {mensaje}";
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            smtp.Port = 587;
+            smtp.Credentials = new NetworkCredential("alfredodanielramos123456@gmail.com", "cxrqgqhylkwprumv"); // Coloca tus credenciales
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
 
             // Redirigir al Index con un ancla para el apartado "CONTACTO"
             return RedirectToAction("Index", new RouteValueDictionary { { "scrollTo", "contacto" } });
-        }
-
-        private void EnviarCorreo(string correoDestino, string nombre, string telefono, string mensaje)
-        {
-            string correoOrigen = "rene230802@gmail.com"; // Cambiar por tu dirección de correo electrónico
-            string contraseña = "Alex32&22rene"; // Cambiar por tu contraseña
-            string servidorSmtp = "smtp.gmail.com"; // Cambiar si estás usando un servidor SMTP diferente
-            int puertoSmtp = 587; // Cambiar el puerto si es necesario
-            bool ssl = true;
-
-            using (MailMessage mail = new MailMessage())
-            {
-                mail.From = new MailAddress(correoOrigen);
-                mail.To.Add(correoDestino);
-                mail.Subject = "Nuevo mensaje de contacto";
-                mail.Body = $"Nombre: {nombre}\nCorreo: {correoDestino}\nTeléfono: {telefono}\nMensaje: {mensaje}";
-
-                using (SmtpClient smtp = new SmtpClient(servidorSmtp, puertoSmtp))
-                {
-                    smtp.UseDefaultCredentials = false;
-                    smtp.Credentials = new NetworkCredential(correoOrigen, contraseña);
-                    smtp.EnableSsl = ssl;
-                    smtp.Send(mail);
-                }
-            }
         }
 
 
