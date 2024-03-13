293249399392e33d5c2d513627e71db3065b8cf9
Rene alejandro Hernandez Ramirez <Alexramires2022@outlook.es>
2024-03-13 09:54:58 -0600

    Access-Job-visual8

---
+++ Access Job/Controllers/FormularioController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using MySql.Data.MySqlClient;
+
+
+namespace Access_Job.Controllers
+{
+    public class FormularioController : Controller
+    {
+        // GET: Formulario
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
--- Access Job/Controllers/HomeController.cs
+++ Access Job/Controllers/HomeController.cs
@@ -1,20 +1,80 @@
 ﻿using System;
 using System.Web.Mvc;
 using System.Data.SqlClient;
+using System.Web.Routing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Net;
-using System.Text;
+using System.Net.Mail;
+
+
+
+
+using MySql.Data.MySqlClient;
 
 namespace Access_Job.Controllers
 {
     public class HomeController : Controller
     {
-        // Cadena de conexión a tu base de datos SQL Server
-        string connectionString = "Server=localhost:3308;Database=formulario_contacto;Trusted_Connection=True;";
-
+        
         public ActionResult Index()
         {
+
+          
             return View();
         }
+
+
+        [HttpPost]
+        public ActionResult Index(string nombre, string correo, string telefono, string mensaje)
+        {
+            string connectionString = "Server=localhost;Port=3308;Database=formulariop;Uid=root;Pwd=;";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = "INSERT INTO contactos (nombre, correo, telefono, mensaje) VALUES (@Nombre, @Correo, @Telefono, @Mensaje)";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Nombre", nombre);
+                command.Parameters.AddWithValue("@Correo", correo);
+                command.Parameters.AddWithValue("@Telefono", telefono);
+                command.Parameters.AddWithValue("@Mensaje", mensaje);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+            // Enviar correo electrónico
+            EnviarCorreo(correo, nombre, telefono, mensaje);
+
+            // Redirigir al Index con un ancla para el apartado "CONTACTO"
+            return RedirectToAction("Index", new RouteValueDictionary { { "scrollTo", "contacto" } });
+        }
+
+        private void EnviarCorreo(string correoDestino, string nombre, string telefono, string mensaje)
+        {
+            string correoOrigen = "rene230802@gmail.com"; // Cambiar por tu dirección de correo electrónico
+            string contraseña = "Alex32&22rene"; // Cambiar por tu contraseña
+            string servidorSmtp = "smtp.gmail.com"; // Cambiar si estás usando un servidor SMTP diferente
+            int puertoSmtp = 587; // Cambiar el puerto si es necesario
+            bool ssl = true;
+
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress(correoOrigen);
+                mail.To.Add(correoDestino);
+                mail.Subject = "Nuevo mensaje de contacto";
+                mail.Body = $"Nombre: {nombre}\nCorreo: {correoDestino}\nTeléfono: {telefono}\nMensaje: {mensaje}";
+
+                using (SmtpClient smtp = new SmtpClient(servidorSmtp, puertoSmtp))
+                {
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new NetworkCredential(correoOrigen, contraseña);
+                    smtp.EnableSsl = ssl;
+                    smtp.Send(mail);
+                }
+            }
+        }
+
 
         public ActionResult About()
         {
@@ -30,69 +90,7 @@ namespace Access_Job.Controllers
             return View();
         }
 
-        // Nuevo método para enviar una solicitud HTTP POST
-        [HttpPost]
-        public ActionResult SendData(string nombre, string correo, string telefono, string mensaje)
-        {
-            try
-            {
-                // Insertar los datos en la tabla "contactos" de la base de datos
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    string query = "INSERT INTO contactos (nombre, correo, telefono, mensaje) VALUES (@nombre, @correo, @telefono, @mensaje)";
-                    SqlCommand command = new SqlCommand(query, connection);
-                    command.Parameters.AddWithValue("@nombre", nombre);
-                    command.Parameters.AddWithValue("@correo", correo);
-                    command.Parameters.AddWithValue("@telefono", telefono);
-                    command.Parameters.AddWithValue("@mensaje", mensaje);
-                    command.ExecuteNonQuery();
-                }
-
-                // Envía la solicitud HTTP POST
-                SendHttpPost(nombre, correo, telefono, mensaje);
-
-                ViewBag.ResponseMessage = "Datos enviados y guardados correctamente.";
-            }
-            catch (Exception ex)
-            {
-                // Manejar cualquier excepción que pueda ocurrir durante el proceso
-                ViewBag.ResponseMessage = "Error: " + ex.Message;
-            }
-
-            // Redirigir de vuelta a la página de inicio
-            return View("Index");
-        }
-
-        // Método para enviar la solicitud HTTP POST
-        private void SendHttpPost(string nombre, string correo, string telefono, string mensaje)
-        {
-            // URL del script PHP en tu servidor XAMPP que recibirá los datos
-            string url = "http://localhost/insertar_contacto.php";
-
-            // Construir los datos a enviar
-            string postData = $"nombre={nombre}&correo={correo}&telefono={telefono}&mensaje={mensaje}";
-
-            // Codificar los datos para enviarlos en la solicitud
-            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
-
-            // Crear una solicitud HTTP POST
-            WebRequest request = WebRequest.Create(url);
-            request.Method = "POST";
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.ContentLength = byteArray.Length;
-
-            // Escribir los datos en el cuerpo de la solicitud
-            using (var stream = request.GetRequestStream())
-            {
-                stream.Write(byteArray, 0, byteArray.Length);
-            }
-
-            // Obtener la respuesta del servidor
-            using (WebResponse response = request.GetResponse())
-            {
-                // No es necesario procesar la respuesta aquí, pero podrías hacerlo si lo necesitas
-            }
+      
         }
     }
-}
+
